f1f795defd7299ee4df3c0538b954a7b38c0e600
weilbach <weilbach@umich.edu>
2019-09-11 17:07:11 -0700

    okay it finally all compiles and the refactoring has been completed

--- mlhelper.py
+++ mlhelper.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
-
-from dayrank import generate_feature_matrix, extract_dictionary
+import string
 
 
 def load_data(fname):
@@ -36,6 +35,81 @@ def load_data(fname):
 #     X_test = project1.generate_feature_matrix(X_test, dictionary)
 
 #     return (X_train, Y_train, X_test, Y_test, dictionary)
+
+def extract_dictionary(df):
+    """
+    Reads a panda dataframe, and returns a dictionary of distinct words
+    mapping from each distinct word to its index (ordered by when it was found).
+    Input:
+        df: dataframe/output of load_data()
+    Returns:
+        a dictionary of distinct words that maps each distinct word
+        to a unique index corresponding to when it was first found while
+        iterating over all words in each review in the dataframe df
+    """
+    # count = 0
+    text_list = df['text']
+    word_dict = {}
+    count = 0
+
+    for line in text_list:
+
+        # line = remove_punctuation(line) #don't need this anymore 
+        line = line.lower()
+        words = line.split(' ')
+        for word in words:
+            if word != '':
+                if word not in word_dict:
+                    word_dict[word] = count
+                    count += 1
+    
+    # print(word_dict)
+
+    return word_dict
+
+
+def generate_feature_matrix(df, word_dict):
+    """
+    Reads a dataframe and the dictionary of unique words
+    to generate a matrix of {1, 0} feature vectors for each review.
+    Use the word_dict to find the correct index to set to 1 for each place
+    in the feature vector. The resulting feature matrix should be of
+    dimension (number of reviews, number of words).
+    Input:
+        df: dataframe that has the ratings and labels
+        word_list: dictionary of words mapping to indices
+    Returns:
+        a feature matrix of dimension (number of reviews, number of words)
+    """
+    number_of_reviews = df.shape[0]
+    number_of_words = len(word_dict)
+    word_list = df['text']
+    feature_matrix = np.zeros((number_of_reviews, number_of_words))
+    count = 0
+
+    for line in word_list:
+        line = remove_punctuation(line)
+        line = line.lower()
+        words = line.split(' ')
+        for word in words:
+            if word in word_dict:
+                feature_matrix[count][word_dict[word]] = 1
+        count += 1
+
+
+    return feature_matrix
+
+def remove_punctuation(value):
+    #this is a remnant from an old project, it works but is likely slow
+    result = ''
+    for c in value:
+        # If char is not punctuation, add it to the result.
+        if c not in string.punctuation:
+            result += c
+        else:
+            c = ' '
+            result += c
+    return result
 
 
 
@@ -92,10 +166,10 @@ def get_multiclass_training_data():
          1: good
     Also returns the dictionary used to create X_train.
     """
-    fname = "summer2018badlabels.csv"
+    fname = "summer2018labels.csv"
     dataframe = load_data(fname)
-    dictionary = dayrank.extract_dictionary(dataframe)
-    X_train = dayrank.generate_feature_matrix(dataframe, dictionary)
+    dictionary = extract_dictionary(dataframe)
+    X_train = generate_feature_matrix(dataframe, dictionary)
     Y_train = dataframe['label'].values.copy()
 
     return (X_train, Y_train, dictionary)
@@ -111,7 +185,7 @@ def get_heldout_reviews(dictionary):
     """
     fname = "summer15.csv"
     dataframe = load_data(fname)
-    X = dayrank.generate_feature_matrix(dataframe, dictionary)
+    X = generate_feature_matrix(dataframe, dictionary)
     return X
 
 
